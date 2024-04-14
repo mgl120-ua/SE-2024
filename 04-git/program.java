92a6dc194b5d9ca86333f77986c70ce7bc5ca7ff
marta <martagrimaldoslopez@gmail.com>
2024-04-14 19:34:55 +0200

    task 5

--- 04-git/program.java
+++ 04-git/program.java
@@ -1,5 +1,8 @@
+//task 6
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello new-branch!");
+        System.out.println("Hello!");
+        System.out.println("Hello task 5!");
+        System.out.println("Hello world!");
     }
 }
