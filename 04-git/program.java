14091301fcefe257ad221ab690360618a1c39e78
marta <martagrimaldoslopez@gmail.com>
2024-04-15 13:05:02 +0200

    updated 04-git

--- 04-git/program.java
+++ 04-git/program.java
@@ -2,7 +2,7 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("Hello task 5!");
+        System.out.println("Hello task 7!");
         System.out.println("Hello world!");
     }
 }
--- 04-git/readme.md
+++ 04-git/readme.md
@@ -1,3 +1,4 @@
 14/04/2024
 Marta Grimaldos López
 ER1556
+task 7
