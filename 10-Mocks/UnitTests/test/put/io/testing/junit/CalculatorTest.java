1d2f376f267f8e6fd0b7e6803da4d4b8ae5b64b4
marta <martagrimaldoslopez@gmail.com>
2024-05-06 17:39:25 +0200

    Task 1

---
+++ 10-Mocks/UnitTests/test/put/io/testing/junit/CalculatorTest.java
@@ -0,0 +1,27 @@
+package put.io.testing.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        int result1 = calculator.add(2, 5);
+        assertEquals(7, result1);
+        
+        int result2 = calculator.add(-2, 2);
+        assertEquals(0, result2);
+    }
+
+    @Test
+    public void testMultiply() {
+        int result1 = calculator.multiply(3, 4);
+        assertEquals(12, result1);
+
+        int result2 = calculator.multiply(-2, 5);
+        assertEquals(-10, result2);
+    }
+}
