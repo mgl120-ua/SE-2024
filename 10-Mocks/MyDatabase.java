a562fd0ae1b5ca689af558835f69d354ff86718b
martagrimaldos <78533630+mgl120-ua@users.noreply.github.com>
2024-05-20 16:30:41 +0200

    Add files via upload

---
+++ 10-Mocks/MyDatabase.java
@@ -0,0 +1,24 @@
+package put.io.testing.mocks;
+
+import put.io.students.fancylibrary.database.IFancyDatabase;
+import java.util.Collections;
+import java.util.List;
+
+public class MyDatabase implements IFancyDatabase {
+    @Override
+    public void connect() {
+    }
+
+    @Override
+    public <T> List<T> queryAll() {
+        return Collections.emptyList();  
+    }
+
+    @Override
+    public void persist(Object o) {
+    }
+
+    @Override
+    public void close() {
+    }
+}
